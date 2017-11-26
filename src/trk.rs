0ebd3ae8db8821bd5bb2eed2c45f1b178cfc208d
Nil Goyette <nil.goyette@imeka.ca>
2017-11-26 14:39:03 -0500

    Use trait Default

--- src/trk.rs
+++ src/trk.rs
@@ -1,5 +1,4 @@
 
-// use std::mem::size_of;
 use std::fs::{File};
 use std::io::{BufReader, BufWriter, Read, Seek, SeekFrom, Write};
 use std::slice::from_raw_parts;
@@ -14,23 +13,23 @@ use streamlines::{Streamlines, Point};
 #[repr(C, packed)]
 pub struct Header {
     pub id_string: [u8; 6],
-    pub dim: [i16; 3],
-    pub voxel_size: [f32; 3],
-    pub origin: [f32; 3],
-    pub n_scalars: i16,
-    pub scalar_name: [u8; 200],    // [10][20]
-    pub n_properties: i16,
-    pub property_name: [u8; 200],  // [10][20]
-    pub vox_to_ras: [f32; 16],     // [4][4]
+    pub dim: [i16; 3],                              // *
+    pub voxel_size: [f32; 3],                       // *
+    pub origin: [f32; 3],                           // *
+    pub n_scalars: i16,                             // *
+    pub scalar_name: [u8; 200],    // [10][20]      // *
+    pub n_properties: i16,                          // *
+    pub property_name: [u8; 200],  // [10][20]      // *
+    pub vox_to_ras: [f32; 16],     // [4][4]        // *
     pub reserved: [u8; 444],
-    pub voxel_order: [u8; 4],
+    pub voxel_order: [u8; 4],                       // *
     pub pad2: [u8; 4],
-    pub image_orientation_patient: [f32; 6],
+    pub image_orientation_patient: [f32; 6],        // *
     pub pad1: [u8; 2],
-    pub invert_x: u8,
+    pub invert_x: u8,                               // *
     pub invert_y: u8,
     pub invert_z: u8,
-    pub swap_x: u8,
+    pub swap_x: u8,                                 // *
     pub swap_y: u8,
     pub swap_z: u8,
     pub n_count: i32,
@@ -38,7 +37,38 @@ pub struct Header {
     pub hdr_size: i32
 }
 
-const HEADER_SIZE: usize = 1000;  // size_of::<Header>();
+// TODO Use size_of::<Header>() when possible
+// use std::mem::size_of;
+const HEADER_SIZE: usize = 1000;
+
+impl Default for Header {
+    fn default() -> Header {
+         Header {
+            id_string: *b"TRACK\0",
+            dim: [1, 1, 1],
+            voxel_size: [1.0, 1.0, 1.0],
+            origin: [0.0, 0.0, 0.0],
+            n_scalars: 0,
+            scalar_name: [0; 200],
+            n_properties: 0,
+            property_name: [0; 200],
+            vox_to_ras: [1.0, 0.0, 0.0, 0.0,
+                         0.0, 1.0, 0.0, 0.0,
+                         0.0, 0.0, 1.0, 0.0,
+                         0.0, 0.0, 0.0, 1.0],
+            reserved: [0; 444],
+            voxel_order: [82, 65, 83, 0],
+            pad2: [0; 4],
+            image_orientation_patient: [1.0, 0.0, 0.0, 0.0, 1.0, 0.0],
+            pad1: [0; 2],
+            invert_x: 0, invert_y: 0, invert_z: 0,
+            swap_x: 0, swap_y: 0, swap_z: 0,
+            n_count: 0,
+            version: 2,
+            hdr_size: HEADER_SIZE as i32
+        }
+    }
+}
 
 impl Header {
     pub fn get_scalar(&self, i: usize) -> &str {
@@ -171,33 +201,10 @@ pub fn write_streamlines(streamlines: &Streamlines, path: &str) {
     let mut writer = BufWriter::new(f);
 
     {
-        // TODO Create a default!
         let header = Header {
-            id_string: *b"TRACK\0",
-            dim: [1, 1, 1],                         // From header
-            voxel_size: [1.0, 1.0, 1.0],            // From header
-            origin: [0.0, 0.0, 0.0],                // From header
-            n_scalars: 0,                           // From header
-            scalar_name: [0; 200],                  // From header
-            n_properties: 0,                        // From header
-            property_name: [0; 200],                // From header
-            vox_to_ras: [1.0, 0.0, 0.0, 0.0,        // From header
-                         0.0, 1.0, 0.0, 0.0,
-                         0.0, 0.0, 1.0, 0.0,
-                         0.0, 0.0, 0.0, 1.0],
-            reserved: [0; 444],
-            voxel_order: [82, 65, 83, 0],           // From header
-            pad2: [0; 4],
-            image_orientation_patient:              // From header
-                [1.0, 0.0, 0.0, 0.0, 1.0, 0.0],
-            pad1: [0; 2],
-            invert_x: 0, invert_y: 0, invert_z: 0,  // From header
-            swap_x: 0, swap_y: 0, swap_z: 0,        // From header
             n_count: streamlines.lengths.len() as i32,
-            version: 2,
-            hdr_size: 1000
+            ..Header::default()
         };
-
         let bytes = unsafe {
             from_raw_parts(&header as *const Header as *const u8, HEADER_SIZE)
         };
