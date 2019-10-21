c81464462778c7b19e75ccb0e1b94eef84f60a72
Nil Goyette <nil.goyette@imeka.ca>
2019-10-21 11:33:24 -0400

    add_property tests

--- tests/header.rs
+++ tests/header.rs
@@ -33,3 +33,35 @@ fn test_unicode_scalar() {
     let mut header = Header::default();
     header.add_scalar("平仮名, ひらがな").unwrap();
 }
+
+#[test]
+fn test_add_property() {
+    let torsion = "torsion".to_string();
+
+    let mut header = Header::default();
+    header.add_property(&torsion).unwrap();
+    assert_eq!(header.properties_name, vec![torsion])
+}
+
+#[test]
+#[should_panic]
+fn test_too_much_properties() {
+    let mut header = Header::default();
+    for _ in 0..11 {
+        header.add_property("test").unwrap();
+    }
+}
+
+#[test]
+#[should_panic]
+fn test_property_name_too_long() {
+    let mut header = Header::default();
+    header.add_property("01234567890123456789a").unwrap();
+}
+
+#[test]
+#[should_panic]
+fn test_unicode_property() {
+    let mut header = Header::default();
+    header.add_property("平仮名, ひらがな").unwrap();
+}
