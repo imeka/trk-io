00cf753278880a49952e4a9a61392957753e299f
Nil Goyette <nil.goyette@imeka.ca>
2019-10-21 12:35:58 -0400

    copy_scalars_and_properties test

--- tests/header.rs
+++ tests/header.rs
@@ -3,6 +3,28 @@ extern crate trk_io;
 use trk_io::Header;
 
 #[test]
+fn test_copy_scalars_and_properties() {
+    let s1 = "s1".to_string();
+    let s2 = "s2".to_string();
+    let p1 = "p1".to_string();
+    let p2 = "p2".to_string();
+
+    let mut header1 = Header::default();
+    header1.add_scalar(&s2).unwrap();
+    header1.add_property(&p2).unwrap();
+
+    let mut header2 = Header::default();
+    header2.add_scalar(&s1).unwrap();
+    header2.add_scalar(&s2).unwrap();
+    header2.add_property(&p1).unwrap();
+    header2.add_property(&p2).unwrap();
+
+    header2.copy_scalars_and_properties(&header1);
+    assert_eq!(header2.scalars_name, vec![s2]);
+    assert_eq!(header2.properties_name, vec![p2]);
+}
+
+#[test]
 fn test_add_scalar() {
     let torsion = "torsion".to_string();
 
