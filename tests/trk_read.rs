1a6bf9623808c5921801094025053a02877d7117
Nil Goyette <nil.goyette@imeka.ca>
2022-12-05 09:15:29 -0500

    Add tests

--- tests/trk_read.rs
+++ tests/trk_read.rs
@@ -27,9 +27,9 @@ fn test_load_simple() {
         Point::new(12.0, 13.0, 14.0),
     ];
 
+    // Test the complete tractogram reading
     let Tractogram { streamlines, scalars, properties } =
         Reader::new("data/simple.trk").unwrap().tractogram();
-
     assert_eq!(streamlines.len(), 3);
     assert_eq!(streamlines[0], first);
     assert_eq!(streamlines[1], second);
@@ -37,17 +37,32 @@ fn test_load_simple() {
     assert!(scalars.is_empty());
     assert!(properties.is_empty());
 
-    // Test generator
+    // Test the complete points reading
+    let streamlines = Reader::new("data/simple.trk").unwrap().points();
+    assert_eq!(streamlines.len(), 3);
+    assert_eq!(streamlines[0], first);
+    assert_eq!(streamlines[1], second);
+    assert_eq!(streamlines[2], third);
+
+    // Test the tractogram items generator
     let reader = Reader::new("data/simple.trk").unwrap();
     for (i, (streamline, _, _)) in reader.into_iter().enumerate() {
-        if i == 0 {
-            assert_eq!(streamline, first);
-        } else if i == 1 {
-            assert_eq!(streamline, second);
-        } else if i == 2 {
-            assert_eq!(streamline, third);
-        } else {
-            panic!("Failed test.");
+        match i {
+            0 => assert_eq!(streamline, first),
+            1 => assert_eq!(streamline, second),
+            2 => assert_eq!(streamline, third),
+            _ => panic!("Failed test."),
+        }
+    }
+
+    // Test the streamlines generator
+    let reader = Reader::new("data/simple.trk").unwrap();
+    for (i, streamline) in reader.streamlines().enumerate() {
+        match i {
+            0 => assert_eq!(streamline, first),
+            1 => assert_eq!(streamline, second),
+            2 => assert_eq!(streamline, third),
+            _ => panic!("Failed test."),
         }
     }
 }
@@ -176,29 +191,15 @@ fn check_complex_scalars_and_properties(
     );
     assert_eq!(&scalars[0], &[1.0, 0.0, 0.0, 0.200000003]);
     assert_eq!(&scalars[1], &[0.0, 1.0, 0.0, 0.300000012, 0.0, 1.0, 0.0, 0.400000006]);
+    #[rustfmt::skip]
     assert_eq!(
         &scalars[2],
         &[
-            0.0,
-            0.0,
-            1.0,
-            0.500000000,
-            0.0,
-            0.0,
-            1.0,
-            0.600000024,
-            0.0,
-            0.0,
-            1.0,
-            0.600000024,
-            0.0,
-            0.0,
-            1.0,
-            0.699999988,
-            0.0,
-            0.0,
-            1.0,
-            0.800000012
+            0.0, 0.0, 1.0, 0.500000000,
+            0.0, 0.0, 1.0, 0.600000024,
+            0.0, 0.0, 1.0, 0.600000024,
+            0.0, 0.0, 1.0, 0.699999988,
+            0.0, 0.0, 1.0, 0.800000012
         ]
     );
 
